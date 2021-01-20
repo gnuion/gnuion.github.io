69687fac8b9ca032f867c78e4fc298b914d9daf4
gnuion <gnu.ion@gmail.com>
2021-01-20 10:56:29 +0100

    some_modifications

--- background-generator/script.js
+++ background-generator/script.js
@@ -2,7 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.querySelector("body");
-
+css.textContent = body.style.background +";";
 
 function setGradient() {
 	body.style.background = 
