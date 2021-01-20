4ae72001517e1ca59655cc38c74676c13c986f7d
gnuion <gnu.ion@gmail.com>
2021-01-20 11:01:12 +0100

    required

--- background-generator/script.js
+++ background-generator/script.js
@@ -2,7 +2,6 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.querySelector("body");
-css.textContent = body.style.background +";";
 
 function setGradient() {
 	body.style.background = 
@@ -14,6 +13,8 @@ function setGradient() {
 
 		css.textContent = body.style.background +";";
 }
+
+setGradient();
 
 // color1.addEventListener("input", setGradient);
 
